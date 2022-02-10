e47b1f644754362e3291992b9e7ac298f8dcb87d
Escolano Théo <escolano725@gmail.com>
2022-02-10 16:47:27 +0100

    fin de cours

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -3,25 +3,47 @@
 namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
-use App\Entity\BlogPost;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 final class UserDataPersister implements ContextAwareDataPersisterInterface
 {
-    public function supports($data, array $context = []): bool
+    private $userPasswordEncoder;
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $userPasswordEncoder)
     {
-        return $data instanceof BlogPost;
+        $this->entityManager = $entityManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
     }
 
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User;
+    }
+
+    /**
+     * @param User $data
+     */
     public function persist($data, array $context = [])
     {
-        
-		
-		
-        return $data;
+        if ($this->userPasswordEncoder->needsRehash($data)) {
+            $data->setPassword(
+                $this->userPasswordEncoder->encodePassword($data, $data->getPassword())
+            );
+            $data->eraseCredentials();
+        }
+
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
     }
 
     public function remove($data, array $context = [])
     {
-        // call your persistence layer to delete $data
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
     }
 }
